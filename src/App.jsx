ccadb6aaf48628d46c0ee45b6d9ba0bfcf546988
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-09-10 03:35:56 +0530

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const App = () => {
+function App() {
+    const [workouts, setWorkouts] = useState([])
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:4000/api/workouts')
+            .then((res) => {
+                console.log(res.data)
+                setWorkouts(res.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }, [])
+
     return (
         <>
-            <div>App</div>
+            <div className="">
+                {workouts.length > 0 ? (
+                    workouts.map((workout, _) => (
+                        <div className="mb-4" key={workout?._id}>
+                            <h4 className="">{workout?.title}</h4>
+                            <p className="">Reps: {workout?.reps}</p>
+                            <p className="">Load: {workout?.load}</p>
+
+                            <div className="mt-4">
+                                <h4 className="">doc details</h4>
+                                <p className="">ID: {workout?._id}</p>
+                                <p className="">
+                                    Created At: {workout?.createdAt}
+                                </p>
+                                <p className="">
+                                    Updated At: {workout?.updatedAt}
+                                </p>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>No data to show</p>
+                )}
+            </div>
         </>
     )
 }
