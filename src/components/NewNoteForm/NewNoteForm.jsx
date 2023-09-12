057bb70dee2662c5c796f020db8e217f25e6df69
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-09-12 21:48:30 +0530

    Add new note form component

---
+++ src/components/NewNoteForm/NewNoteForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NewNoteForm = () => {
+  return (
+    <>
+    <form className=''>
+        <div className="">
+            <label htmlFor="">Title</label>
+        </div>
+    </form>
+    </>
+  )
+}
+
+export default NewNoteForm
