8092b8573837e7af5a265360420f24453cb549bc
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-09-12 21:49:16 +0530

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,17 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { Suspense } from 'react'
+import { Routes } from 'react-router-dom'
+import { Layout } from './components'
 
 function App() {
-    const [workouts, setWorkouts] = useState([])
-
-    useEffect(() => {
-        axios
-            .get('http://localhost:4000/api/workouts')
-            .then((res) => {
-                console.log(res.data)
-                setWorkouts(res.data)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }, [])
-
     return (
         <>
-            <div className="">
-                {workouts.length > 0 ? (
-                    workouts.map((workout, _) => (
-                        <div className="mb-4" key={workout?._id}>
-                            <h4 className="">{workout?.title}</h4>
-                            <p className="">Reps: {workout?.reps}</p>
-                            <p className="">Load: {workout?.load}</p>
-
-                            <div className="mt-4">
-                                <h4 className="">doc details</h4>
-                                <p className="">ID: {workout?._id}</p>
-                                <p className="">
-                                    Created At: {workout?.createdAt}
-                                </p>
-                                <p className="">
-                                    Updated At: {workout?.updatedAt}
-                                </p>
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    <p>No data to show</p>
-                )}
+            <div className="min-h-screen bg-[#f1f1f1] flex flex-col">
+                <Suspense fallback={'Loading...'}>
+                    <Layout>
+                        {/* <Routes></Routes> */}
+                        hello
+                    </Layout>
+                </Suspense>
             </div>
         </>
     )
