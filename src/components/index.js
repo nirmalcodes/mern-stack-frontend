65b32ce864b175e16021fcb573476da18fef9390
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-09-17 03:37:58 +0530

    Update components index

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import { lazy } from 'react'
 
 const Layout = lazy(() => import('./Layout/Layout'))
 const NewNoteForm = lazy(() => import('./NewNoteForm/NewNoteForm'))
+const NoteCard = lazy(() => import('./NoteCard/NoteCard'))
+const MasonryGrid = lazy(() => import('./MasonryGrid/MasonryGrid'))
 
-export { Layout, NewNoteForm }
+export { Layout, NewNoteForm, NoteCard, MasonryGrid }
